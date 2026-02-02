a13fc39e20526b8dfa72dcf2f0531c407b2071c3
Curtis <cjododev@gmail.com>
2026-02-02 00:13:19 -0700

    add logging

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,7 @@
 const POST_API = import.meta.env.POST_API
 
 export const getPost = async (ref: string) => {
+	console.log(POST_API)
 	const res = await fetch(`${POST_API}/post/${ref}`, {
 		cache: "no-store"
 	});
