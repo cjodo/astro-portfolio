0977f2393f835f6f6a4328be69895310b49a3898
Curtis <cjododev@gmail.com>
2026-02-01 22:02:00 -0700

    add github stars to project card

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,31 @@ export const calculateReadingTime = (text: string, wpm: number = 225): number =>
 	const readTime = Math.ceil(minutes);
 	return readTime;
 }
+
+export const getRepoStarCount = async (repo: string) => {
+	const res = await fetch(`https://api.github.com/repos/cjodo/${repo}`);
+	const data = await res.json();
+
+	const stars = data["stargazers_count"];
+	return stars
+}
+
+export const extractRepoName = (url: string) => {
+	try {
+		// Remove any trailing slash
+		url = url.replace(/\/$/, "");
+
+		// Match the part after github.com/username/
+		const match = url.match(/github\.com\/[^\/]+\/([^\/]+)$/);
+		if (match) {
+			return match[1];
+		} else {
+			throw new Error("Invalid GitHub URL");
+		}
+	} catch (e: any) {
+		console.error(e.message);
+		return null;
+	}
+
+
+}
